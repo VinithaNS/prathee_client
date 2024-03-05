b20951b5329f62260aa271cf619cc8e5e7537b2d
vinithaneyashree <vinithaneyashree2901@gmail.com>
2024-03-05 21:08:49 +0530

    latest commit

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-export const API = "http://localhost:8000";
-// export const API = "https://company-task-server.onrender.com";
+// export const API = "http://localhost:8000";
+export const API = "https://pratheee-task-a8ma.onrender.com";
